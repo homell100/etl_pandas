d145cd3c92006ab926606ebb59905a7f2e915b9b
homell100 <your-email@example.com>
2025-06-25 15:37:24 +0200

    Updated read_users_excel function to pass successful test & adding successful test

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -10,7 +10,7 @@ sys.path.append(str(parent_dir))
 
 from etl.extract import read_products_csv, read_users_excel
 
-class TestExtractFunctions():
+class TestExtractCSVFunctions():
 
     expected_df = pd.DataFrame({
     "product_id": [1],
@@ -46,3 +46,40 @@ class TestExtractFunctions():
             dtype=self.expected_data_types
         )
         assert df is None
+
+class TestExtractExcelFunctions():
+
+    mock_dfs = {
+    0: pd.DataFrame({
+        "id": [1],
+        "email": ["a@a.com"],
+        "signup_date": pd.to_datetime(["2024-01-01"])
+        }),
+    1: pd.DataFrame({
+        "id": [1],
+        "email": ["b@b.com"],
+        "signup_date": pd.to_datetime(["2024-02-01"])
+        })
+    }
+    expected_df = pd.DataFrame({
+        "id":[1,1],
+        "email": ["a@a.com", "b@b.com"],
+        "signup_date": pd.to_datetime(["2024-01-01", "2024-02-01"])
+    }) 
+    expected_data_types = {
+        "id": np.int16,
+        "email": 'S10'
+    }
+    test_path = "test_path_excel.xslx"
+
+    @patch("etl.extract.pd.read_excel", return_value=mock_dfs)
+    def test_read_products_csv_success(self, mock_read_excel):
+        """Test read_products_csv response"""
+        df = read_users_excel(self.test_path)
+        pd.testing.assert_frame_equal(df, self.expected_df)
+        mock_read_excel.assert_called_once_with(
+            self.test_path,
+            sheet_name=[0,1],
+            parse_dates=["signup_date"],
+            dtype=self.expected_data_types
+        )
